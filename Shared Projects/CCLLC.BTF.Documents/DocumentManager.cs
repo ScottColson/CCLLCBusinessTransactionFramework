f19f04276935c1ccf7c58e9d300a8b2489e8d71c
Scott Colson <scott@colsoncode.com>
2020-02-15 07:01:17 -0800

    Add shared projects for document management support.

---
+++ Shared Projects/CCLLC.BTF.Documents/DocumentManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CCLLC.BTF.Documents.CDS
+{
+    using CCLLC.Core;
+
+    public class DocumentManager : IDocumentManager
+    {
+        public IList<IGeneratedDocument> LoadGeneratedDocuments(IProcessExecutionContext executionContext, Guid transactionId, TimeSpan? CacheTimeout = null)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
---
+++ Shared Projects/CCLLC.BTF.Documents/Interfaces/IGeneratedDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CCLLC.BTF.Documents
+{
+    using CCLLC.Core;
+    using CCLLC.BTF.Platform;
+
+    public interface IGeneratedDocument : IRecordPointer<Guid>
+    {       
+        IRecordPointer<Guid> TransactionId { get; }
+        ICustomer Customer { get; }
+    }
+}
