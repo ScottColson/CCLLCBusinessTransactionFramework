7f12bcf0ff6ea3dafd8997ace4271455f8f45b8c
Scott Colson <scott@colsoncode.com>
2020-02-27 17:02:39 -0800

    Update plugin container registrations based on recent changes to business and data layer components.

--- CDS/CCLLC.BTF.Process.CDSSolution/Plugins/PluginBase.cs
+++ CDS/CCLLC.BTF.Process.CDSSolution/Plugins/PluginBase.cs
@@ -4,9 +4,8 @@ using CCLLC.CDS.Sdk;
 using CCLLC.BTF.Platform;
 using CCLLC.BTF.Platform.CDS;
 using CCLLC.BTF.Revenue;
+using CCLLC.BTF.Revenue.CDS;
 using CCLLC.BTF.Documents;
-
-
 
 namespace CCLLC.BTF.Process.CDS.Plugins
 {
@@ -44,23 +43,29 @@ namespace CCLLC.BTF.Process.CDS.Plugins
             // Register implementation classes from the model libraries. The container will 
             // resolve these implementations as needed for dependency injection.
             this.Container.Implement<IAgentFactory>().Using<AgentFactory>().AsSingleInstance();
-            this.Container.Implement<IAlternateBranchFactory>().Using<AlternateBranchFactory>().AsSingleInstance();
-            this.Container.Implement<IAppliedFeeManager>().Using<AppliedFeeManager>().AsSingleInstance();
-            this.Container.Implement<ITransactionContextFactory>().Using<TransactionContextFactory>().AsSingleInstance();
+            this.Container.Implement<IAlternateBranchFactory>().Using<AlternateBranchFactory>().AsSingleInstance();            
             this.Container.Implement<ICustomerFactory>().Using<CustomerFactory>().AsSingleInstance();
             this.Container.Implement<IDeferredActivator>().Using<DeferredActivator>().AsSingleInstance();
             this.Container.Implement<IDeficiencyManager>().Using<DeficiencyManager>().AsSingleInstance();
             this.Container.Implement<IDocumentManager>().Using<DocumentManager>().AsSingleInstance();
             this.Container.Implement<IEvidenceManager>().Using<EvidenceManager>().AsSingleInstance();
+            this.Container.Implement<IFeeList>().Using<LazyFeeList>().AsSingleInstance();
             this.Container.Implement<ILocationFactory>().Using<LocationFactory>().AsSingleInstance();
             this.Container.Implement<ILogicEvaluatorFactory>().Using<LogicEvaluatorFactory>().AsSingleInstance();
             this.Container.Implement<ILogicEvaluatorTypeFactory>().Using<LogicEvaluatorTypeFactory>().AsSingleInstance();
             this.Container.Implement<IParameterSerializer>().Using<DefaultSerializer>().AsSingleInstance();
+            this.Container.Implement<IPlatformDataConnector>().Using<PlatformDataConnector>();
             this.Container.Implement<IPlatformManager>().Using<PlatformManager>().AsSingleInstance();
+            this.Container.Implement<IPriceCalculatorFactory>().Using<PriceCalculatorFactory>();
             this.Container.Implement<IProcessStepFactory>().Using<ProcessStepFactory>().AsSingleInstance();
             this.Container.Implement<IProcessStepTypeFactory>().Using<ProcessStepTypeFactory>().AsSingleInstance();
             this.Container.Implement<IRequirementEvaluator>().Using<DefaultRequirementEvaluator>().AsSingleInstance();
-            this.Container.Implement<IStepHistoryManager>().Using<StepHistoryManager>().AsSingleInstance();
+            this.Container.Implement<IRevenueDataConnector>().Using<RevenueDataConnector>().AsSingleInstance();
+            this.Container.Implement<IStepHistoryDataConnector>().Using<StepHistoryDataConnector>().AsSingleInstance();
+            this.Container.Implement<ITransactionContextFactory>().Using<TransactionContextFactory>().AsSingleInstance();
+            this.Container.Implement<ITransactionDataConnector>().Using<TransactionDataConnector>().AsSingleInstance(); 
+            this.Container.Implement<ITransactionFeeListFactory>().Using<TransactionFeeListFactory>().AsSingleInstance();
+            this.Container.Implement<ITransactionHistoryFactory>().Using<TransactionHistoryFactory>().AsSingleInstance();
             this.Container.Implement<ITransactionManagerFactory>().Using<TransactionManagerFactory>().AsSingleInstance();    
             this.Container.Implement<ITransactionProcessFactory>().Using<TransactionProcessFactory>().AsSingleInstance();
             this.Container.Implement<ITransactionRequirementFactory>().Using<TransactionRequirementFactory>().AsSingleInstance();
